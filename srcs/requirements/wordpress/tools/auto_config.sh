9531ad644f116618db39597059d21988b63fd573
emajuri <emajuri@emajuri.emajuri.42.fr>
2024-03-06 18:01:00 +0200

    Some fixes

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -5,21 +5,24 @@ wp config create --allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	--dbhost=mariadb:3306 \
+	--path='/var/www/wordpress'
 
-wp core install --title=myTitle \
+wp core install --allow-root \
+	--title=myTitle \
 	--admin_user=$WP_ADMIN \
 	--admin_password=$WP_ADMIN_PASS  \
 	--admin_email=$WP_ADMIN_EMAIL \
 	--skip-email
 
-wp user create $WP_USER \
+wp user create --allow-root \
+	$WP_USER \
 	$WP_USER_EMAIL \
 	--user_pass=$WP_USER_PASS
 
 path="/run/php"
 
-if [! -d "$path" ]; then
+if [ ! -d "$path" ]; then
 	mkdir -p "$path"
 fi
 
